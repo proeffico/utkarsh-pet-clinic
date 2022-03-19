2e20cba80f2b1276f8a831466a1846203de25502
Utkarsh Yadav <utkarsh.yadav.cs2019@iimtindia.net>
2022-03-19 14:52:22 +0530

    Initial Project

---
+++ src/main/java/com/utkarsh/utkpetclinic/UtkPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.utkarsh.utkpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtkPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UtkPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/utkarsh/utkpetclinic/UtkPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utkarsh.utkpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtkPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
