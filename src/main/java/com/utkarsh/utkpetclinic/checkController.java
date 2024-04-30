3f3e0f875908022ee7c16b9cdfa22c299802ce4c
Utkarsh Yadav <utkarsh@proeffico.com>
2024-04-30 16:34:49 +0530

    Hello api

---
+++ src/main/java/com/utkarsh/utkpetclinic/checkController.java
@@ -0,0 +1,19 @@
+package com.utkarsh.utkpetclinic;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class checkController {
+
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello(){
+        return "<h1>Hello World!!!</h1>";
+    }
+
+
+}
